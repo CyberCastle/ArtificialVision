2e40bd9ac3eb89dce3fe2f355b685544446e3414
CyberCastle <CyberCastle@gmail.com>
2014-07-16 23:47:45 -0400

    Se añade proyecto Netbeans con una demostración de OpenCV bajo Java

---
+++ OpenCV_Test-One/src/main/java/cl/cc/main/Run.java
@@ -0,0 +1,115 @@
+package cl.cc.main;
+
+import java.io.File;
+import java.net.URL;
+import org.bytedeco.javacpp.Loader;
+import static org.bytedeco.javacpp.helper.opencv_objdetect.cvHaarDetectObjects;
+import static org.bytedeco.javacpp.opencv_core.CV_AA;
+import org.bytedeco.javacpp.opencv_core.CvMemStorage;
+import org.bytedeco.javacpp.opencv_core.CvPoint;
+import org.bytedeco.javacpp.opencv_core.CvRect;
+import org.bytedeco.javacpp.opencv_core.CvScalar;
+import org.bytedeco.javacpp.opencv_core.CvSeq;
+import static org.bytedeco.javacpp.opencv_core.IPL_DEPTH_8U;
+import org.bytedeco.javacpp.opencv_core.IplImage;
+import static org.bytedeco.javacpp.opencv_core.cvClearMemStorage;
+import static org.bytedeco.javacpp.opencv_core.cvFillConvexPoly;
+import static org.bytedeco.javacpp.opencv_core.cvGetSeqElem;
+import static org.bytedeco.javacpp.opencv_core.cvLoad;
+import static org.bytedeco.javacpp.opencv_core.cvPoint;
+import static org.bytedeco.javacpp.opencv_core.cvRectangle;
+import static org.bytedeco.javacpp.opencv_imgproc.CV_BGR2GRAY;
+import static org.bytedeco.javacpp.opencv_imgproc.cvCvtColor;
+import org.bytedeco.javacpp.opencv_objdetect;
+import static org.bytedeco.javacpp.opencv_objdetect.CV_HAAR_DO_CANNY_PRUNING;
+import org.bytedeco.javacpp.opencv_objdetect.CvHaarClassifierCascade;
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.FrameGrabber;
+
+/**
+ *
+ * @author bytedeco
+ * Code obtain from here: https://github.com/bytedeco/javacv
+ * NOTA: Se ha copiado sólo el código necesario para el detector de rostros
+ */
+public class Run {
+
+    public static void main(String[] args) throws Exception {
+        
+        String classifierName;
+        if (args.length > 0) {
+            classifierName = args[0];
+        } else {
+            URL url = new URL("https://raw.github.com/Itseez/opencv/2.4/data/haarcascades/haarcascade_frontalface_alt.xml");
+            File file = Loader.extractResource(url, null, "classifier", ".xml");
+            file.deleteOnExit();
+            classifierName = file.getAbsolutePath();
+        }
+
+        // Preload the opencv_objdetect module to work around a known bug.
+        Loader.load(opencv_objdetect.class);
+
+        // We can "cast" Pointer objects by instantiating a new object of the desired class.
+        CvHaarClassifierCascade classifier = new CvHaarClassifierCascade(cvLoad(classifierName));
+        if (classifier.isNull()) {
+            System.err.println("Error loading classifier file \"" + classifierName + "\".");
+            System.exit(1);
+        }
+
+        // The available FrameGrabber classes include OpenCVFrameGrabber (opencv_highgui),
+        // DC1394FrameGrabber, FlyCaptureFrameGrabber, OpenKinectFrameGrabber,
+        // PS3EyeFrameGrabber, VideoInputFrameGrabber, and FFmpegFrameGrabber.
+        FrameGrabber grabber = FrameGrabber.createDefault(0);
+        grabber.start();
+
+        // FAQ about IplImage:
+        // - For custom raw processing of data, getByteBuffer() returns an NIO direct
+        //   buffer wrapped around the memory pointed by imageData, and under Android we can
+        //   also use that Buffer with Bitmap.copyPixelsFromBuffer() and copyPixelsToBuffer().
+        // - To get a BufferedImage from an IplImage, we may call getBufferedImage().
+        // - The createFrom() factory method can construct an IplImage from a BufferedImage.
+        // - There are also a few copy*() methods for BufferedImage<->IplImage data transfers.
+        IplImage grabbedImage = grabber.grab();
+        int width = grabbedImage.width();
+        int height = grabbedImage.height();
+        IplImage grayImage = IplImage.create(width, height, IPL_DEPTH_8U, 1);
+
+        // Objects allocated with a create*() or clone() factory method are automatically released
+        // by the garbage collector, but may still be explicitly released by calling release().
+        // You shall NOT call cvReleaseImage(), cvReleaseMemStorage(), etc. on objects allocated this way.
+        CvMemStorage storage = CvMemStorage.create();
+
+        // CanvasFrame is a JFrame containing a Canvas component, which is hardware accelerated.
+        // It can also switch into full-screen mode when called with a screenNumber.
+        // We should also specify the relative monitor/camera response for proper gamma correction.
+        CanvasFrame frame = new CanvasFrame("Sonria!!!!!! :-)", CanvasFrame.getDefaultGamma() / grabber.getGamma());
+
+        // We can allocate native arrays using constructors taking an integer as argument.
+        CvPoint hatPoints = new CvPoint(3);
+
+        while (frame.isVisible() && (grabbedImage = grabber.grab()) != null) {
+            cvClearMemStorage(storage);
+
+            // Let's try to detect some faces! but we need a grayscale image...
+            cvCvtColor(grabbedImage, grayImage, CV_BGR2GRAY);
+            CvSeq faces = cvHaarDetectObjects(grayImage, classifier, storage,
+                    1.1, 3, CV_HAAR_DO_CANNY_PRUNING);
+            int total = faces.total();
+            for (int i = 0; i < total; i++) {
+                CvRect r = new CvRect(cvGetSeqElem(faces, i));
+                int x = r.x(), y = r.y(), w = r.width(), h = r.height();
+                cvRectangle(grabbedImage, cvPoint(x, y), cvPoint(x + w, y + h), CvScalar.RED, 1, CV_AA, 0);
+
+                // To access or pass as argument the elements of a native array, call position() before.
+                hatPoints.position(0).x(x - w / 10).y(y - h / 10);
+                hatPoints.position(1).x(x + w * 11 / 10).y(y - h / 10);
+                hatPoints.position(2).x(x + w / 2).y(y - h / 2);
+                cvFillConvexPoly(grabbedImage, hatPoints.position(0), 3, CvScalar.GREEN, CV_AA, 0);
+            }
+
+            frame.showImage(grabbedImage);
+        }
+        frame.dispose();
+        grabber.stop();
+    }
+}
