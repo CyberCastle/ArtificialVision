7088a6ff740dfe216e64d26aabf87ff5154dccf1
CyberCastle <CyberCastle@gmail.com>
2014-08-30 21:15:32 -0400

    Add OpenCV + CUDA demo

---
+++ OpenCV_Cuda_Test/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=opencv_cuda_test
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/opencv_cuda_test
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=opencvcudatest.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/opencvcudatest.tar
+# Release configuration
+CND_PLATFORM_Release=CLang-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/CLang-MacOSX
+CND_ARTIFACT_NAME_Release=opencv_cuda_test
+CND_ARTIFACT_PATH_Release=dist/Release/CLang-MacOSX/opencv_cuda_test
+CND_PACKAGE_DIR_Release=dist/Release/CLang-MacOSX/package
+CND_PACKAGE_NAME_Release=opencvcudatest.tar
+CND_PACKAGE_PATH_Release=dist/Release/CLang-MacOSX/package/opencvcudatest.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
